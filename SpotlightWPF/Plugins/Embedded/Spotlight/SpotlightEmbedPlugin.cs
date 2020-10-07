31dac2063315c2000999d3a814afe8946caeda9f
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2020-10-07 21:01:46 +0300

    Added icon for spotlight plugin

---
+++ SpotlightWPF/Plugins/Embedded/Spotlight/SpotlightEmbedPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Winspotlight.Models;
+
+namespace Winspotlight.Plugins.Embedded
+{
+    public class SpotlightEmbedPlugin : PluginCore
+    {
+        private Bitmap spotlightIcon;
+
+        public SpotlightEmbedPlugin()
+        {
+            spotlightIcon = PluginHelper.LoadImage("Spotlight", "Images/main.png") as Bitmap;
+        }
+
+        public override string GetName()
+        {
+            return "Spotlight";
+        }
+
+        public override IEnumerable<SearchItem> SearchItems(string searchText)
+        {
+            return new List<SearchItem>()
+            {
+                new SearchDelegateItem("Close", "Spotlight", (item) => MainWindow.CloseWindow()) { iconBitmap = spotlightIcon },
+                new SearchDelegateItem("Settings", "Spotlight", (item) => MainWindow.OpenSettings()) { iconBitmap = spotlightIcon },
+                new SearchDelegateItem("Reindex files", "Spotlight", (item) => Index()) { iconBitmap = spotlightIcon },
+                new SearchDelegateItem("Sleep", "Spotlight", (item) => Application.SetSuspendState(PowerState.Suspend, true, true)) { iconBitmap = spotlightIcon },
+                new SearchDelegateItem("Hibernate", "Spotlight", (item) => Application.SetSuspendState(PowerState.Hibernate, true, true)) { iconBitmap = spotlightIcon },
+            };
+        }
+    }
+}
---
+++ SpotlightWPF/Plugins/PluginHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+
+namespace Winspotlight.Plugins
+{
+    public static class PluginHelper
+    {
+        /// <summary>
+        /// Load image from application pack
+        /// </summary>
+        /// <param name="relativePath">Plugin folder relative path (<b>example:</b> /Images/icon.png)</param>
+        public static Image LoadImage(string pluginName, string relativePath)
+        {
+            return Image.FromStream(Application.GetResourceStream(new System.Uri("pack://application:,,,/Plugins/Embedded/" + pluginName + "/" + relativePath)).Stream);
+        }
+    }
+}
